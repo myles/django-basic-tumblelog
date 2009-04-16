104d0df99ac8671adf79638a79c0d801723d6264
Myles Braithwaite <me@mylesbraithwaite.com>
2009-04-16 14:06:48 -0400

    Added the ChessyShop stuff.

--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
+import os
 from setuptools import setup, find_packages
+
+def read(fname):
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
 	name = "django-basic-tumblelog",
-	version = "1.0",
+	version = "0.6",
 	url = 'http://myles.github.com/django-basic-tumblelog/',
 	license = 'Apache 2.0',
 	description = "A basic tumblelog application you can add to your Django website.",
+	long_description = read('README'),
+	
 	author = 'Myles Braithwaite',
+	author_email = 'me@mylesbraithwaite.com',
+	
 	packages = find_packages('src'),
 	package_dir = {'': 'src'},
+	
 	install_requires = ['setuptools'],
+	
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Framework :: Django',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: Apache 2.0',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Topic :: Internet :: WWW/HTTP',
+	]
 )
